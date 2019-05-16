97473bede472b1a8024f5da97e4709e6a418f40c
Ryan Ho <ryandonaldho@gmail.com>
2019-05-16 18:14:30 -0500

    Added more data

---
+++ brandmapping.py
@@ -0,0 +1,13 @@
+# data-store-type
+id_to_brand = {
+    '18': 'tigerdirect',
+    '3': 'walmart',
+    '5': 'amazon',
+    '11': 'homedepot',
+    '12': 'bestbuy',
+    '10': 'lowes',
+    '13': 'macy',
+    '8': 'officedepot',
+    '1': 'target'
+
+}
--- main.py
+++ main.py
@@ -1,30 +1,53 @@
+from brandmapping import id_to_brand
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.chrome.options import Options
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-#for Ubuntu driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
+# for Ubuntu driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=chrome_options)
 
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get("https://brickseek.com/deals/")
+driver.get("https://brickseek.com/deals/?sort=newest")
 driver.implicitly_wait(3)
-filter = driver.find_element_by_xpath("//select[@id='banner-sort']/option[@value='newest']").click()
-
-# https://brickseek.com/deals/?sort=newest&pg=59
+# filter = driver.find_element_by_xpath("//select[@id='banner-sort']/option[@value='newest']").click()
 
 # get number of pages
 number_pages = int(driver.find_element_by_class_name('total').text)
 print(number_pages)
 
-for page in range(1,number_pages+1):
+for page in range(1, number_pages + 1):
     driver.get(f'https://brickseek.com/deals/?sort=newest&pg={page}')
     sleep(2)
     # scrape page
     deals = driver.find_elements_by_class_name('item-list__item')
     for deal in deals:
-        title = deal.find_element_by_class_name('item-list__title')
-        print(title.text)
+        # get title
+        title = deal.find_element_by_class_name('item-list__title').text
+        # get current price
+        current_dollar = deal.find_elements_by_class_name('price-formatted__dollars')[0].text
+        current_dollar = int(current_dollar.replace(',', ''))
+
+        current_cents = float(deal.find_elements_by_class_name('price-formatted__cents')[0].text)
+        current_price = current_dollar + current_cents / 100
+        # get previous price
+        previous_dollar = deal.find_elements_by_class_name('price-formatted__dollars')[1].text
+        previous_dollar = int(previous_dollar.replace(',', ''))
+
+        previous_cents = float(deal.find_elements_by_class_name('price-formatted__cents')[1].text)
+        previous_price = previous_dollar + previous_cents / 100
+
+        # get store brand
+        id = deal.find_element_by_class_name('item-list__store').get_attribute('data-store-type')
+        store_brand = id_to_brand[id] if id in id_to_brand else 'none'
+
+        # image url
+        image_url = deal.find_element_by_class_name('item-list__image-container')\
+            .find_element_by_tag_name('img').get_attribute('src')
+
+        format_data = f'{title} {current_price} {previous_price} {store_brand} {image_url}'
+        print(format_data)
 
 driver.close()
